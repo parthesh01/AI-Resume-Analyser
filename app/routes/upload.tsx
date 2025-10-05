bed5471c0ddf3a4bfd71e125d56d6ad09b2771f1
NIGGERTRON\Parthesh <parthesh01@gmail.com>
2025-10-05 12:30:41 +0530

    Debug 1

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,4 +1,5 @@
-import { prepareInstructions } from 'constants';
+
+import { prepareInstructions } from '../../constants';
 import React, { useState, type FormEvent } from 'react'
 import { useNavigate } from 'react-router';
 import FileUploader from '~/components/FileUploader';
